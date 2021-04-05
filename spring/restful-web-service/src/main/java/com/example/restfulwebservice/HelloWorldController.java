8497edf74bc0c8fdba426a895ec192d20834ea7b
juhwan0815 <juwom0831@naver.com>
2021-04-05 14:54:45 +0900

    SpringBoot를 이용한 Restful Web Services 개발 : Spring Boot로 개발하는 RESTFUL Service

---
+++ spring/restful-web-service/src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.example.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    // GET
+    // /hello-world (endPoint)
+    // @RequestMapping(method=RequestMethod.GET, path="/hello-world")
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping("/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+}
