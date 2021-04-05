707cee4c748f912282af3a71527de98270092d90
juhwan0815 <juwom0831@naver.com>
2021-04-06 02:04:34 +0900

    SpringBoot를 이용한 Restful Web Services 개발 : Spring Boot API 사용

--- spring/restful-web-service/src/main/java/com/example/restfulwebservice/user/UserController.java
+++ spring/restful-web-service/src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,5 +1,7 @@
 package com.example.restfulwebservice.user;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -7,6 +9,10 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.methodOn;
+
 
 @RestController
 public class UserController {
@@ -24,14 +30,18 @@ public class UserController {
 
     // GET /users/1 or /users/10 -> String
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public EntityModel<User> retrieveUser(@PathVariable int id){
         User user = service.findOne(id);
 
         if(user == null){
             throw new UserNotFoundException(String.format("ID[%s] not found",id));
         }
 
-        return user;
+        // HATEOAS
+        EntityModel<User> model = new EntityModel<>(user);
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        model.add(linkTo.withRel("all-users"));
+        return model;
     }
 
     @PostMapping("/users")
