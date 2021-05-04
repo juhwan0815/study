af581d1b515219307ed37d2d596b9f24ece99e71
juhwan0815 <juwom0831@naver.com>
2021-05-04 13:22:23 +0900

    코드로 배우는 스프링부트 웹 프로젝트 : 영화 리뷰 프로젝트 생성

---
+++ spring/springbootwebproject/mreview/src/main/java/com/example/mreview/MreviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.mreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MreviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MreviewApplication.class, args);
+    }
+
+}
