c980228854ed97e37082c6e44a8dcfac060f7452
juhwan0815 <juwom0831@naver.com>
2021-05-05 19:30:15 +0900

    코드로 배우는 스프링부트 웹 프로젝트 : 스프링시큐리티를 이용한 로그인 처리

---
+++ spring/springbootwebproject/club/src/test/java/com/example/club/ClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
