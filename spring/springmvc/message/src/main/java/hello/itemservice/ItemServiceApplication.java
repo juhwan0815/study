cad8fb8a4ecee838c8c9bc456d4b984e2126fbbd
juhwan0815 <juwom0831@naver.com>
2021-06-26 20:41:36 +0900

    스프링 MVC - 백엔드 웹 개발 활용 기술 : 메세지, 국제화

---
+++ spring/springmvc/message/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -0,0 +1,22 @@
+package hello.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceApplication.class, args);
+	}
+
+//	@Bean
+//	public MessageSource messageSource() {
+//		ResourceBundleMessageSource messageSource = new
+//				ResourceBundleMessageSource();
+//		messageSource.setBasenames("messages", "errors");
+//		messageSource.setDefaultEncoding("utf-8");
+//		return messageSource;
+//	}
+
+}
