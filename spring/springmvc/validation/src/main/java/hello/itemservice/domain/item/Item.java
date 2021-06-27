6b1d7dca8d9003d97e2275e253d4d41e72fed567
juhwan0815 <juwom0831@naver.com>
2021-06-27 15:30:18 +0900

    스프링 MVC - 백엔드 웹 개발 활용 기술 : 검증1 - Validation

---
+++ spring/springmvc/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,35 @@
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class Item {
+
+
+    private Long id;
+
+    @NotBlank(message = "공백X")
+    private String itemName;
+
+    @NotNull
+    @Range(min = 1000,max = 1000000)
+    private Integer price;
+
+    @NotNull
+    @Max(9999)
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
