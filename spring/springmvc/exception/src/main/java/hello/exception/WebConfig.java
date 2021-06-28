5ec236859b536dc3788af5f3e74dc455d512d421
juhwan0815 <juwom0831@naver.com>
2021-06-29 03:33:08 +0900

    스프링 MVC - 백엔드 웹 개발 활용 기술 : 예외처리와 오류 페이지

---
+++ spring/springmvc/exception/src/main/java/hello/exception/WebConfig.java
@@ -0,0 +1,33 @@
+package hello.exception;
+
+import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+            .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**","*.ico","/error","/error-page/**"); // 오류 페이지 경로
+    }
+
+//    @Bean
+    public FilterRegistrationBean logFilter(){
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+//        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST,DispatcherType.ERROR);
+        return filterRegistrationBean;
+    }
+}
