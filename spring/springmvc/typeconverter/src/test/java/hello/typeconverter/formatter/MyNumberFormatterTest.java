68bd6d0c435f6bde3dc2c99ea89b04e852fda647
juhwan0815 <juwom0831@naver.com>
2021-07-11 17:41:11 +0900

    스프링 MVC - 백엔드 웹 개발 활용 기술 복습 : 스프링 타입 컨버터

---
+++ spring/springmvc/typeconverter/src/test/java/hello/typeconverter/formatter/MyNumberFormatterTest.java
@@ -0,0 +1,27 @@
+package hello.typeconverter.formatter;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyNumberFormatterTest {
+
+    MyNumberFormatter  formatter = new MyNumberFormatter();
+
+    @Test
+    void parse() throws ParseException {
+        Number result = formatter.parse("1,000", Locale.KOREA);
+
+        assertThat(result).isEqualTo(1000L); // Long 타입 주의
+    }
+
+    @Test
+    void print(){
+        String result = formatter.print(1000, Locale.KOREA);
+        assertThat(result).isEqualTo("1,000");
+    }
+}
