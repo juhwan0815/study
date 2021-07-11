e02730b9b377080864c98d70f6d17be29dff4e9c
juhwan0815 <juwom0831@naver.com>
2021-07-12 03:32:06 +0900

    스프링 MVC - 백엔드 웹 개발 활용 기술 복습 : 파일 업로드

---
+++ spring/springmvc/upload/src/main/java/hello/upload/controller/ItemController.java
@@ -0,0 +1,88 @@
+package hello.upload.controller;
+
+import hello.upload.domain.Item;
+import hello.upload.domain.ItemRepository;
+import hello.upload.domain.UploadFile;
+import hello.upload.file.FileStore;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.buf.UriUtil;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriUtils;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemRepository itemRepository;
+    private final FileStore fileStore;
+
+    @GetMapping("/items/new")
+    public String newItem(@ModelAttribute ItemForm form){
+        return "item-form";
+    }
+
+    @PostMapping("/items/new")
+    public String saveItem(@ModelAttribute ItemForm form, RedirectAttributes redirectAttributes) throws IOException {
+        UploadFile attachFile = fileStore.storeFile(form.getAttachFile());
+        List<UploadFile> storeImageFiles = fileStore.storeFiles(form.getImageFiles());
+
+        // 데이터베이스에 저장
+        Item item = new Item();
+        item.setItemName(form.getItemName());
+        item.setAttachFile(attachFile);
+        item.setImageFiles(storeImageFiles);
+
+        itemRepository.save(item);
+
+        redirectAttributes.addAttribute("itemId",item.getId());
+
+        return "redirect:/items/{itemId}";
+    }
+
+    @GetMapping("/items/{id}")
+    public String items(@PathVariable Long id, Model model){
+        Item item = itemRepository.findById(id);
+        model.addAttribute("item",item);
+        return "item-view";
+    }
+
+    @ResponseBody
+    @GetMapping("/images/{filename}")
+    public Resource downloadImage(@PathVariable String filename) throws MalformedURLException {
+        // file:/Users/../5f66762d-dcb7-40ba-818e-1a8e6c23ed01.png
+        return new UrlResource("file:" + fileStore.getFullPath(filename));
+    }
+
+    @GetMapping("/attach/{itemId}")
+    public ResponseEntity<Resource> downloadAttach(@PathVariable Long itemId) throws MalformedURLException {
+        Item item = itemRepository.findById(itemId);
+        String storeFileName = item.getAttachFile().getStoreFileName();
+        String uploadFileName = item.getAttachFile().getUploadFileName();
+
+        UrlResource resource = new UrlResource("file:" + fileStore.getFullPath(storeFileName));
+
+        log.info("uploadFileName={}",uploadFileName);
+
+        String encodedUploadFileName = UriUtils.encode(uploadFileName, StandardCharsets.UTF_8);
+        String contentDisposition = "attachment; filename=\"" + encodedUploadFileName + "\"";
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,contentDisposition)
+                .body(resource);
+
+    }
+}
---
+++ spring/springmvc/upload/src/main/java/hello/upload/domain/UploadFile.java
@@ -0,0 +1,16 @@
+package hello.upload.domain;
+
+import lombok.Data;
+
+@Data
+public class UploadFile {
+
+    private String uploadFileName;
+
+    private String storeFileName;
+
+    public UploadFile(String uploadFileName, String storeFileName) {
+        this.uploadFileName = uploadFileName;
+        this.storeFileName = storeFileName;
+    }
+}
