17cbbb392060996fad7e72abaef499f041c2a894
juhwan0815 <juwom0831@naver.com>
2021-07-31 14:37:00 +0900

    Vue.js 시작하기 - 프로젝트 생성 도구 - Vue-CLI

---
+++ Vue.js/Vue.js-start/vue-cli/src/a.vue
@@ -0,0 +1,19 @@
+<template>
+<!-- HTML -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+    // Javascript - 인스턴스 옵션
+    methods: {
+        addNum: function(){
+
+        }
+    }
+}
+</script>
+
+<style>
+/* CSS */
+</style>
