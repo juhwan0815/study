a166feb2330d798b730f1713c95dbd5340ecdd97
juhwan0815 <juwom0831@naver.com>
2021-08-04 13:11:23 +0900

    Vue.js 시작하기 복습 - 싱글 파일 컴포넌트

--- Vue.js/Vue.js-start/vue-cli/src/App.vue
+++ Vue.js/Vue.js-start/vue-cli/src/App.vue
@@ -1,34 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <!-- 컴포넌트 명명법 종류 -->
-<!--    <hello-world></hello-world>-->
-<!--    <HelloWorld></HelloWorld>-->
-<!--    <HelloWorld/>-->
+  <div>
+<!--    <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header>-->
+    <app-header
+        v-bind:propsdata="str"
+        v-on:renew="renewStr"></app-header>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from "./components/AppHeader";
+
+// var appHeader = {
+//   template: '<header><h1>Header</h1></header>'
+// }
+
+// new Vue({
+//   data: {
+//     str: 'hi'
+//   }
+// })
 
 export default {
-  // 인스턴스 옵션 속성 or 컴포넌트 옵션 속성
-  name: 'App',
+  data: function (){
+    return {
+      str: 'Header'
+    }
+  },
   components: {
-    HelloWorld,
-    // 'hello-world': HelloWorld
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function () {
+      this.str = 'hi';
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
