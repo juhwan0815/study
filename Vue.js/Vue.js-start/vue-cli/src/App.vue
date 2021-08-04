a424d4c009c970ac22793c6a5e0382f77613a7f3
juhwan0815 <juwom0831@naver.com>
2021-08-04 11:12:33 +0900

    Vue.js 시작하기 복습 - 프로젝트 생성 도구 - Vue CLI

---
+++ Vue.js/Vue.js-start/vue-cli/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 컴포넌트 명명법 종류 -->
+<!--    <hello-world></hello-world>-->
+<!--    <HelloWorld></HelloWorld>-->
+<!--    <HelloWorld/>-->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  // 인스턴스 옵션 속성 or 컴포넌트 옵션 속성
+  name: 'App',
+  components: {
+    HelloWorld,
+    // 'hello-world': HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue.js/Vue.js-start/vue-cli/src/a.vue
@@ -0,0 +1,27 @@
+<!--var appHeader= {-->
+<!--  template: '<div></div>',-->
+<!--  methods: {-->
+<!--    addNum: function() {-->
+<!--    -->
+<!--    }-->
+<!--  }-->
+<!--}-->
+
+<template>
+<!--HTML-->
+</template>
+
+<script>
+export default {
+  // Javascript - 인스턴스 옵션
+  methods: {
+    addNum: function() {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+/*CSS*/
+</style>
