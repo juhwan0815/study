57ce0589ddc9b0853ae8f82f8e5af6fea93a011a
juhwan0815 <juwom0831@naver.com>
2021-08-06 01:23:05 +0900

    Vue.js 중급 강좌 : Todo App - 프로젝트 구현

--- Vue.js/Vue.js-intermediate/vue-todo/src/App.vue
+++ Vue.js/Vue.js-intermediate/vue-todo/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
   </div>
 </template>
 
@@ -10,12 +13,44 @@ import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 import TodoFooter from "./components/TodoFooter";
 
+// var my_cmp = {
+//   template: '<div><my component/div>'
+// }
+//
+// new Vue({
+//   el:'',
+//   components: {
+//     'my-cmp': my_cmp
+//   }
+// });
 
 export default {
-
+  components: {
+    // 컴포넌트 태그명 : 컴포넌트 내용
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter,
+  }
 }
 </script>
 
 <style>
+body {
+  text-align: center;
+  background-color: #F6F6F6;
+}
 
+input {
+  border-style: groove;
+  width: 200px;
+}
+
+button {
+  border-style: groove;
+}
+
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
 </style>
