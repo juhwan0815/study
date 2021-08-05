632f20658b014d5386031faa4f83a569bfe0c482
juhwan0815 <juwom0831@naver.com>
2021-08-05 11:50:04 +0900

    Vue.js 중급 강좌 : Todo App - 프로젝트 소개 및 구성

---
+++ Vue.js/Vue.js-intermediate/vue-todo/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+import TodoHeader from "./components/TodoHeader";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import TodoFooter from "./components/TodoFooter";
+
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
