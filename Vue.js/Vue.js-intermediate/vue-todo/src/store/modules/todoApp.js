8a30b39c5818db3a988304a56ec8523aa7a9ba67
juhwan0815 <juwom0831@naver.com>
2021-08-14 12:38:45 +0900

    Vue.js 중급 강좌 복습 : Vuex - 프로젝트 구조화 및 모듈화

---
+++ Vue.js/Vue.js-intermediate/vue-todo/src/store/modules/todoApp.js
@@ -0,0 +1,51 @@
+const storage = {
+    fetch() {
+        const arr = [];
+        if (localStorage.length > 0) {
+            for (let i = 0; i < localStorage.length; i++) {
+                if (localStorage.key(i) !== 'loglevel:webpack-dev-server')
+                    arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+            }
+        }
+        return arr;
+    },
+}
+
+const state =  {
+    todoItems: storage.fetch()
+}
+
+const getters = {
+    storedTodoItems(state) {
+        return state.todoItems;
+    }
+};
+
+const mutations = {
+    addOneItem(state, todoItem) {
+        const obj = {completed: false, item: todoItem};
+        localStorage.setItem(todoItem,JSON.stringify(obj));
+        state.todoItems.push(obj);
+    },
+    removeOneItem(state,payload){
+        localStorage.removeItem(payload.todoItem.item);
+        state.todoItems.splice(payload.index, 1);
+    },
+    toggleOneItem(state,payload){
+        state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed
+        // 로컬 스토리지의 데이터를 갱신
+        localStorage.removeItem(payload.todoItem.item);
+        localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
+    },
+    clearAllItem(state){
+        localStorage.clear();
+        state.todoItems = [];
+    }
+};
+
+export default {
+    state,
+    getters,
+    mutations
+}
+
