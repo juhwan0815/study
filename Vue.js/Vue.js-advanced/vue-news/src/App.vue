fb19072c5aefeb32a67c038519876a0fb93ff419
juhwan0815 <juwom0831@naver.com>
2021-08-19 21:51:26 +0900

    Vue.js 완벽가이드 복습 : 프로젝트 셋업

---
+++ Vue.js/Vue.js-advanced/vue-news/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    Hello
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    fetchData() {
+      console.log('hello');
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
