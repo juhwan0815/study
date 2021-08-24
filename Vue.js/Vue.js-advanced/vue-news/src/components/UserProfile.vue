732d9419c5127738845098ecd2c24feb9d51721e
juhwan0815 <juwom0831@naver.com>
2021-08-24 23:24:18 +0900

    Vue.js 완벽가이드 복습 : 리팩토링2 - 사용자 프로필 컴포넌트 공통화

---
+++ Vue.js/Vue.js-advanced/vue-news/src/components/UserProfile.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="user-container">
+    <div>
+      <i class="fas fa-user"></i>
+    </div>
+    <div class="user-description">
+      <!-- 상위 컴포넌트에서 정의할 영역 -->
+      <slot name="username"></slot>
+      <div class="time">
+        <!-- 상위 컴포넌트에서 정의할 영역 -->
+        <slot name="time"></slot>
+      </div>
+      <!-- 상위 컴포넌트에서 정의할 영역 -->
+      <slot name="karma"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    info: Object
+  },
+}
+</script>
+
+<style scoped>
+
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+
+.fa-user {
+  font-size: 2.5rem;
+}
+
+.user-description {
+  padding-left: 8px;
+}
+
+.time {
+  font-size: 0.7rem;
+}
+</style>
--- Vue.js/Vue.js-advanced/vue-news/src/views/ItemView.vue
+++ Vue.js/Vue.js-advanced/vue-news/src/views/ItemView.vue
@@ -1,20 +1,12 @@
 <template>
   <div>
     <section>
-      <!--질문 상세 정보 -->
-      <div class="user-container">
-        <div>
-          <i class="fas fa-user"></i>
-        </div>
-        <div class="user-description">
-          <router-link :to="`/user/${fetchedItem.user}`">
-            {{ fetchedItem.user }}
-          </router-link>
-          <div class="time">
-            {{ fetchedItem.time_ago }}
-          </div>
-        </div>
-      </div>
+      <UserProfile :info="fetchedItem">
+        <router-link slot="username" :to="`/user/${fetchedItem.user}`">{{ fetchedItem.user }}</router-link>
+        <template slot="time">{{'Posted ' + fetchedItem.time_ago }}</template>
+      </UserProfile>
+    </section>
+    <section>
       <h2>{{ fetchedItem.title }}</h2>
     </section>
     <section>
@@ -25,9 +17,13 @@
 </template>
 
 <script>
+import UserProfile from "../components/UserProfile";
 import {mapGetters} from 'vuex'
 
 export default {
+  components: {
+    UserProfile
+  },
   computed: {
     ...mapGetters(['fetchedItem'])
   },
@@ -44,12 +40,15 @@ export default {
   align-items: center;
   padding: 0.5rem;
 }
+
 .fa-user {
   font-size: 2.5rem;
 }
+
 .user-description {
   padding-left: 8px;
 }
+
 .time {
   font-size: 0.7rem;
 }
