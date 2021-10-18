42baefc81b4ec750d53d123a86f7ca10a7ae7e36
juhwan0815 <juwom0831@naver.com>
2021-10-18 20:51:07 +0900

    Redis : strings, lists, hashs, sets, sortedSets, geo, hyperloglog 복습

--- spring/redis/src/main/java/com/redis/study/redis/repository/redis/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.redis.study.redis.repository.redis;
-
-import com.redis.study.redis.domain.redis.Student;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepository extends CrudRepository<Student,Long> {
-}
--- spring/redis/src/main/java/spring/study/redis/RedisApplication.java
+++ spring/redis/src/main/java/spring/study/redis/RedisApplication.java
@@ -1,4 +1,4 @@
-package com.redis.study.redis;
+package spring.study.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
