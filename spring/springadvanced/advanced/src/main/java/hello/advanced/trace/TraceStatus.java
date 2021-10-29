d811429fd9db6da964e1e6a16c5f8b575c2491c2
juhwan0815 <juwom0831@naver.com>
2021-10-30 00:24:15 +0900

    스프링 핵심 원리 - 고급편 : 예제 만들기

---
+++ spring/springadvanced/advanced/src/main/java/hello/advanced/trace/TraceStatus.java
@@ -0,0 +1,16 @@
+package hello.advanced.trace;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TraceStatus {
+
+    private TraceId traceId;
+
+    private Long startTimeMs;
+
+    private String message;
+
+}
