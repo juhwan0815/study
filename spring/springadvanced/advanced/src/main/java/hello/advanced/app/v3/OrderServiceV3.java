6e24cea3f5a0a1a7c6e1a8bcc1380b7a584b529c
juhwan0815 <juwom0831@naver.com>
2021-10-30 23:54:28 +0900

    스프링 핵심 원리 - 고급편 : 쓰레드 로컬

---
+++ spring/springadvanced/advanced/src/main/java/hello/advanced/app/v3/OrderServiceV3.java
@@ -0,0 +1,26 @@
+package hello.advanced.app.v3;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV3 {
+
+    private final OrderRepositoryV3 orderRepository;
+    private final LogTrace trace;
+
+    public void orderItem(String itemId){
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.orderItem()");
+            orderRepository.save(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; //예외를 꼭 다시 던져주어야 한다.
+        }
+    }
+}
