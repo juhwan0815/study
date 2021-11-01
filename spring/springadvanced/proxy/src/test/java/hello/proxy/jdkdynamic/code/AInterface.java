0e1f692710f134d54bbb48170ae125f0b677deea
juhwan0815 <juwom0831@naver.com>
2021-11-01 19:03:03 +0900

    스프링 핵심 원리 - 고급편 : 동적 프록시 기술

---
+++ spring/springadvanced/proxy/src/test/java/hello/proxy/jdkdynamic/code/AInterface.java
@@ -0,0 +1,6 @@
+package hello.proxy.jdkdynamic.code;
+
+public interface AInterface {
+
+    String call();
+}
---
+++ spring/springadvanced/proxy/src/test/java/hello/proxy/jdkdynamic/code/BInterface.java
@@ -0,0 +1,7 @@
+package hello.proxy.jdkdynamic.code;
+
+public interface BInterface {
+
+    String call();
+
+}
