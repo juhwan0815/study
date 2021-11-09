fe33b129273d04815a314a305e1fdbe6ec0d0c92
juhwan0815 <juwom0831@naver.com>
2021-11-09 17:02:57 +0900

    스프링 핵심 원리 - 고급편 : 스프링 AOP - 실전 예제

---
+++ spring/aop/src/main/java/hello/aop/exam/ExamService.java
@@ -0,0 +1,19 @@
+package hello.aop.exam;
+
+import hello.aop.exam.annotation.Trace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.lang.annotation.Target;
+
+@Service
+@RequiredArgsConstructor
+public class ExamService {
+
+    private final ExamRepository examRepository;
+
+    @Trace
+    public void request(String itemId){
+        examRepository.save(itemId);
+    }
+}
