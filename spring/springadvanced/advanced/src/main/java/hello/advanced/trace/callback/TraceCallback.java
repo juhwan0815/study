f6a3026ca26f676d929d270368f25f4954bb2c11
juhwan0815 <juwom0831@naver.com>
2021-11-30 23:32:37 +0900

    스프링 핵심 원리 - 고급편 복습 : 템플릿 메서드 패턴과 콜백 패턴

---
+++ spring/springadvanced/advanced/src/main/java/hello/advanced/trace/callback/TraceCallback.java
@@ -0,0 +1,6 @@
+package hello.advanced.trace.callback;
+
+public interface TraceCallback<T> {
+
+    T call();
+}
---
+++ spring/springadvanced/advanced/src/test/java/hello/advanced/trace/strategy/code/strategy/Strategy.java
@@ -0,0 +1,6 @@
+package hello.advanced.trace.strategy.code.strategy;
+
+public interface Strategy {
+
+    void call();
+}
