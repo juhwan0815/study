7291851487fa5832c37c2710d11e8ff427bee5a8
juhwan0815 <juwom0831@naver.com>
2021-12-05 19:52:23 +0900

    스프링 핵심 원리 - 고급편 복습 : 동적 프록시 기술

--- spring/springadvanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ spring/springadvanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -4,6 +4,8 @@ import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -14,7 +16,9 @@ import org.springframework.context.annotation.Import;
 //@Import(AppV1Config.class)
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class)
+//@Import(ConcreteProxyConfig.class)
+//@Import(DynamicProxyBasicConfig.class)
+@Import(DynamicProxyFilterConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
---
+++ spring/springadvanced/proxy/src/test/java/hello/proxy/jdkdynamic/code/BInterface.java
@@ -0,0 +1,6 @@
+package hello.proxy.jdkdynamic.code;
+
+public interface BInterface {
+
+    String call();
+}
