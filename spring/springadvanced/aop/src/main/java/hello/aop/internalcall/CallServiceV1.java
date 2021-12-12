6672dcffc70e1ebe455e8627fe112805ed668464
juhwan0815 <juwom0831@naver.com>
2021-12-13 01:46:34 +0900

    스프링 핵심 원리 - 고급편 복습 : 스프링 AOP - 실무 주의사항

---
+++ spring/springadvanced/aop/src/main/java/hello/aop/internalcall/CallServiceV1.java
@@ -0,0 +1,27 @@
+package hello.aop.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CallServiceV1 {
+
+    private CallServiceV1 callServiceV1;
+
+    @Autowired
+    public void setCallServiceV1(CallServiceV1 callServiceV1) {
+        log.info("callServiceV1 setter={}" , callServiceV1.getClass());
+        this.callServiceV1 = callServiceV1;
+    }
+
+    public void external(){
+        log.info("call external");
+        callServiceV1.internal();
+    }
+
+    public void internal(){
+        log.info("call internal");
+    }
+}
---
+++ spring/springadvanced/aop/src/test/java/hello/aop/internalcall/CallServiceV0Test.java
@@ -0,0 +1,27 @@
+package hello.aop.internalcall;
+
+import hello.aop.internalcall.aop.CallLogAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Import(CallLogAspect.class)
+@SpringBootTest
+class CallServiceV0Test {
+
+    @Autowired
+    CallServiceV0 callServiceV0;
+
+    @Test
+    void external() {
+        callServiceV0.external();
+    }
+
+    @Test
+    void internal() {
+        callServiceV0.internal();
+    }
+}
