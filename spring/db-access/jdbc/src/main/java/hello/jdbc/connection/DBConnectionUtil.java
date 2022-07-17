43755358c5254c8e776e29771522ff15753ba0a4
juhwan0815 <juwom0831@naver.com>
2022-07-17 15:08:40 +0900

    스프링 DB - 데이터 접근 핵심 원리 : 커넥션풀과 데이터소스 이해

--- spring/db-access/jdbc/src/main/java/hello/jdbc/connection/DBConnectionUtil.java
+++ spring/db-access/jdbc/src/main/java/hello/jdbc/connection/DBConnectionUtil.java
@@ -11,8 +11,7 @@ import static hello.jdbc.connection.ConnectionConst.*;
 @Slf4j
 public class DBConnectionUtil {
 
-    public static Connection
-    getConnection() {
+    public static Connection getConnection() {
         try {
             Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             log.info("get connection={}, class={}", connection, connection.getClass());
