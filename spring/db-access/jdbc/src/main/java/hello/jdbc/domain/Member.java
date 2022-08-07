01a6db5983852fd30cc6ab17372d05f4530ce3bf
juhwan0815 <juwom0831@naver.com>
2022-08-07 15:17:40 +0900

    스프링 DB - 데이터 접근 핵심 원리 복습 : JDBC 이해

---
+++ spring/db-access/jdbc/src/main/java/hello/jdbc/domain/Member.java
@@ -0,0 +1,16 @@
+package hello.jdbc.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+    private String memberId;
+
+    private int money;
+
+}
