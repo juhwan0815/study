f7786a8d33b0c5396c8326c37ee252b82ce01ce8
juhwan0815 <juwom0831@naver.com>
2022-10-06 22:34:11 +0900

    스프링 마이크로서비스 코딩 공작소 : 삭제

--- spring/microservice/organization-service/src/main/java/com/optimagrowth/organizationservice/repository/OrganizationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.optimagrowth.organizationservice.repository;
-
-import com.optimagrowth.organizationservice.model.Organization;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface OrganizationRepository extends JpaRepository<Organization, String> {
-}
