26fef9ed2f3a6e9c044e4d4c50424b7f3d3491c4
juhwan0815 <juwom0831@naver.com>
2022-11-03 19:55:53 +0900

    스프링 DB - 데이터 접근 활용 기술 복습 : 데이터 접근 기술 - 스프링 데이터 JPA

--- spring/db-access/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ spring/db-access/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,6 +2,7 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.jpa.SpringDataJpaItemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,7 +20,8 @@ import javax.sql.DataSource;
 //@Import(JdbcTemplateV2Config.class)
 //@Import(JdbcTemplateV3Config.class)
 //@Import(MyBatisConfig.class)
-@Import(JpaConfig.class)
+//@Import(JpaConfig.class)
+@Import(SpringDataJpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
