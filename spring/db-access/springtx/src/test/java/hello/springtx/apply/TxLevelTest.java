60d4658a42e7d2e371a7fdb6b529c9a89386a0aa
juhwan0815 <juwom0831@naver.com>
2022-11-13 22:27:34 +0900

    스프링 DB - 데이터 접근 활용 기술 복습 : 스프링 트랜잭션 이해

---
+++ spring/db-access/springtx/src/test/java/hello/springtx/apply/TxLevelTest.java
@@ -0,0 +1,57 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@SpringBootTest
+public class TxLevelTest {
+
+    @Autowired
+    LevelService levelService;
+
+    @Test
+    void orderTest() {
+        levelService.write();
+        levelService.read();
+    }
+
+    @TestConfiguration
+    static class TxLevelTestConfig {
+
+        @Bean
+        public LevelService levelService() {
+            return new LevelService();
+        }
+
+    }
+
+    @Slf4j
+    @Transactional(readOnly = true)
+    static class LevelService {
+
+        @Transactional
+        public void write() {
+            log.info("call write");
+            printTxInfo();
+        }
+
+        public void read() {
+            log.info("call read");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+            boolean readOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+            log.info("tx readOnly={}", readOnly);
+        }
+    }
+}
