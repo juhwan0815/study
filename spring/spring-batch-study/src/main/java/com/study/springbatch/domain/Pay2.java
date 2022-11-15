13af2dc32a32e1331928c6ae788d272176651e09
hwangjuhwan <jhanhwang@kicc.co.kr>
2022-11-15 14:07:42 +0900

    스프링 배치 가이드 - ItemWriter

---
+++ spring/spring-batch-study/src/main/java/com/study/springbatch/domain/Pay2.java
@@ -0,0 +1,32 @@
+package com.study.springbatch.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Pay2 {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long amount;
+
+    private String txName;
+
+    private LocalDateTime txDateTime;
+
+    public Pay2(Long amount, String txName, LocalDateTime txDateTime) {
+        this.amount = amount;
+        this.txName = txName;
+        this.txDateTime = txDateTime;
+    }
+}
