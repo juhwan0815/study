a94e8cd25bab723188cd24f3724aa95ef39c5865
hwangjuhwan <jhanhwang@kicc.co.kr>
2022-11-15 14:02:35 +0900

    스프링 배치 가이드 - Batch Job 실행해보기, 메타테이블 엿보기

---
+++ spring/spring-batch-study/src/main/java/com/study/springbatch/BatchApplication.java
@@ -0,0 +1,15 @@
+package com.study.springbatch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class BatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchApplication.class, args);
+    }
+
+}
