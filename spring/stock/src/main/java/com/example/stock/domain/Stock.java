5512203960b7f44ee1437e11086db60caecbae67
juhwan0815 <juwom0831@naver.com>
2023-01-05 20:55:06 +0900

    재고 시스템으로 알아보는 동시성 이슈 해결방법 복습 : 재고시스템 만들어보기

---
+++ spring/stock/src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,34 @@
+package com.example.stock.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "stock_id")
+    private Long id;
+
+    private Long productId;
+
+    private Long quantity;
+
+    public Stock(Long productId, Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public void decrease(Long quantity) {
+        if (this.quantity - quantity < 0) {
+            throw new RuntimeException("foo");
+        }
+
+        this.quantity = this.quantity - quantity;
+    }
+
+}
---
+++ spring/stock/src/main/java/com/example/stock/service/StockService.java
@@ -0,0 +1,25 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class StockService {
+
+    private final StockRepository stockRepository;
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        // get Stock
+        Stock stock = stockRepository.findById(id)
+                .orElseThrow();
+
+        // 재고감소
+        stock.decrease(quantity);
+    }
+
+}
