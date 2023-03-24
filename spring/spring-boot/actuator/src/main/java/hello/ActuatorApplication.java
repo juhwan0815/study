c76c917fafa890255f58358cf1eaa0fd435d675d
juhwan0815 <juwom0831@naver.com>
2023-03-24 23:52:46 +0900

    스프링 부트 - 핵심 원리와 활용 : 액츄에이터

---
+++ spring/spring-boot/actuator/src/main/java/hello/ActuatorApplication.java
@@ -0,0 +1,20 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+    @Bean
+    public HttpExchangeRepository httpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
+    }
+}
