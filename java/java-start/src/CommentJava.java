3edba1883d6cae85948084c907f789907faffd39
juhwan0815 <juwom0831@naver.com>
2024-09-02 17:35:50 +0900

    김영한의 자바 입문 - 코드로 시작하는 자바 첫걸음: Hello World

---
+++ java/java-start/src/CommentJava.java
@@ -0,0 +1,13 @@
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("hello java1"); // hello java1 출력
+        //System.out.println("hello java2");
+
+        /*
+         System.out.println("hello java3");
+         System.out.println("hello java4");
+         */
+    }
+
+}
---
+++ java/java-start/src/HelloJava2.java
@@ -0,0 +1,10 @@
+public class HelloJava2 {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello java1");
+        System.out.println("hello java2");
+        System.out.println("hello java3");
+    }
+
+}
