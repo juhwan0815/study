588c50e3ff36f33cbf511cfd3ea70ec09c9a31e1
juhwan0815 <juwom0831@naver.com>
2025-03-24 19:43:53 +0900

    김영한의 자바 입문 - 코드로 시작하는 자바 첫걸음 : 메서드 중간 커밋

---
+++ java/java-start/src/method/Method1Ref.java
@@ -0,0 +1,20 @@
+package method;
+
+public class Method1Ref {
+
+    public static void main(String[] args) {
+        // 계산 1
+        int sum1 = add(5, 10);
+        System.out.println("결과1 출력: " + sum1);
+
+        // 계산2
+        int sum2 = add(15, 20);
+        System.out.println("결과2 출력: " + sum2);
+    }
+
+    public static int add(int a, int b) {
+        System.out.println(a + "+" + b + " 연산 수행");
+        int sum = a + b;
+        return sum;
+    }
+}
