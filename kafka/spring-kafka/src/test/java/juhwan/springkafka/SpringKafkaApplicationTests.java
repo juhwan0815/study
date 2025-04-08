bf03e5c7187f561732394ee921861a573ca78e53
juhwan0815 <juwom0831@naver.com>
2025-04-08 20:05:22 +0900

    spring kafka docs study : init

---
+++ kafka/spring-kafka/src/test/java/juhwan/springkafka/SpringKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package juhwan.springkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
